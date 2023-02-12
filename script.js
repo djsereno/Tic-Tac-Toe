1963fc0f2fbd6d615e99c034cdb8f618de646b14
djsereno <djsereno91@gmail.com>
2023-02-11 19:56:30 -0800

    Add logic to allow player to fill cells with X's and O's

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const gameBoard = (() => {
-  const boardArray = Array(9).fill('');
+  const _boardArray = Array(9).fill('');
+  let _currentPlayer = 'X';
 
-  const display = () => console.table(boardArray);
+  const display = () => console.table(_boardArray);
+  const getBoardArray = () => [..._boardArray];
+  const getCurrentPlayer = () => _currentPlayer;
 
-  return { boardArray, display };
+  const pickCell = (index) => {
+    if (_boardArray[index] !== '') return false;
+    _boardArray[index] = _currentPlayer;
+    _currentPlayer === 'X' ? (_currentPlayer = 'O') : (_currentPlayer = 'X');
+    return _boardArray[index];
+  };
+
+  return { getBoardArray, getCurrentPlayer, pickCell, display };
 })();
 
 const displayController = (() => {
@@ -11,13 +21,16 @@ const displayController = (() => {
 
   const renderBoard = () => {
     _clearBoard();
-    gameBoard.boardArray.forEach((cellValue) => {
+    const boardArray = gameBoard.getBoardArray();
+    for (let i = 0; i < 9; i++) {
+      const cellValue = boardArray[i];
       const cellNode = document.createElement('li');
       cellNode.classList.add('cell');
+      cellNode.setAttribute('data-index', i);
       cellNode.innerText = cellValue;
       cellNode.addEventListener('click', _pickCell);
       _boardNode.appendChild(cellNode);
-    });
+    }
   };
 
   const _clearBoard = () => {
@@ -27,8 +40,9 @@ const displayController = (() => {
   };
 
   const _pickCell = (event) => {
-    const cell = event.currentTarget;
-    cell.innerText === '' ? (cell.innerText = 'X') : console.log('cell occupied');
+    const index = event.currentTarget.getAttribute('data-index');
+    player = gameBoard.pickCell(+index);
+    if (player) event.currentTarget.innerText = player;
   };
 
   return { renderBoard };
